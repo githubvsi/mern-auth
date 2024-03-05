0e981e8d80d422649878ac48ccb5ebd2d2d5f03f
Wei <wsiuci@gmail.com>
2024-03-04 21:33:53 -0800

    Oops~ here goes the controller registerUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 
 // @desc Auth user/set token
 // route POST /api/user/auth
@@ -17,6 +18,32 @@ const authUser = asyncHandler(async (req, res) => {
 // route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });   // to find a single user that matches the specified value of the key
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists'); // will pass to the custom error middleware
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+
     res.status(200).json({ message: 'Register User'});
 });
 
