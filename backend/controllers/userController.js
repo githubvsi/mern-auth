de25f1eda9dc6dc4039330877ef9daa78caa83d4
Wei <wsiuci@gmail.com>
2024-03-05 18:15:33 -0800

    Logout & destroy cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -69,7 +69,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // route POST /api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Logout User'});
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0),
+    });
+
+    res.status(200).json({ message: 'User logged out'});
 });
 
 
